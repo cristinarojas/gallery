6fe280d2652617c629580f4f977538d5f1ab5c11
Cristina Rojas <telematicacristina@gmail.com>
2015-09-30 12:02:32 -0700

    Adding the Gruntfile and the stylesheets

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  // Loading npm tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    // Sass compilation
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          noCache: true
+        },
+        files: {
+          'public/css/style.css'                : 'sass/style.scss',
+          'public/css/mediaqueries/general.css' : 'sass/mediaqueries/generalmedia.scss',
+        }
+      }
+    },
+    // Watch for sass files changes
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+
+  // Registering default task (execute as 'grunt')
+  grunt.registerTask('default', ['sass']);
+};
